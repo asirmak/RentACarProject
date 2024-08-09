baa93f18af132f06acdec63c8b1e283935471dc8
Ali Sencer Irmak <alisencer.irmak@std.yeditepe.edu.tr>
2024-08-09 15:39:21 +0300

    caching aop

---
+++ Core/Constants/Messages.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Constants
+{
+    public class Messages
+    {
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string WrongValidationType = "Validasyon tipi geçersiz.";
+    }
+}
